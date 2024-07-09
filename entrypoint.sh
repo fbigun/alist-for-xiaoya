76fd927afa06c35c1a8c52b42a59c7d22d8ffba8
FbiGun Deo <4226523+fbigun@users.noreply.github.com>
2024-07-09 21:50:01 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+chown -R ${PUID}:${PGID} /opt/alist/
+
+umask ${UMASK}
+
+while :
+  do
+    CODE=`curl -I -m 10 -o /dev/null -s -w %{http_code}  http://localhost/api/public/settings`
+    if [[ $CODE -eq 200 ]]; then
+      echo -e "\033[42;34m xiaoya_server is ok \033[0m"
+      break
+    else
+      sleep 1
+    fi
+  done
+
+if [ "$1" = "version" ]; then
+  ./alist version
+else
+  exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
+fi
