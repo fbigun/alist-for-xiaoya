b8c1ab00da47ba15ea5864e33c5993fc84ecfdf3
FbiGun Deo <4226523+fbigun@users.noreply.github.com>
2024-07-12 21:23:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ umask ${UMASK}
 
 while :
   do
-    CODE=`curl -I -m 10 -o /dev/null -s -w %{http_code}  http://localhost/api/public/settings`
+    CODE=`curl -I -m 10 -o /dev/null -s -w %{http_code}  http://xiaoya-tvbox/api/public/settings`
     if [[ $CODE -eq 200 ]]; then
       echo -e "\033[42;34m xiaoya_server is ok \033[0m"
       break
