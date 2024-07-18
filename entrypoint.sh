c8c1fc7ef5b16ebae082e7c7f80dc8c66abc8518
FbiGun Deo <4226523+fbigun@users.noreply.github.com>
2024-07-18 21:32:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,10 @@ while :
     CODE=`curl -I -m 10 -o /dev/null -s -w %{http_code}  http://xiaoya-tvbox/api/public/settings`
     if [[ $CODE -eq 200 ]]; then
       echo -e `date +"%Y-%m-%d %H:%M:%S"` "\033[42;34mxiaoya_server is ok.\033[0m"
+      curl -s http://xiaoya-tvbox/api/public/settings
       break
     else
+      curl -s http://xiaoya-tvbox/api/public/settings
       sleep 5
     fi
   done
@@ -19,7 +21,7 @@ while :
 if [ "$1" = "version" ]; then
   ./alist version
 else
-  sleep 120
+  sleep $[10*60]
   echo -e `date +"%Y-%m-%d %H:%M:%S"` "\033[42;34m启动 alist 服务.\033[0m"
   exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
 fi
